87df415e6cc8f94a29a2a8322f61820119d3a77b
Łukasz Gębicki <kontakt@gebickionline.pl>
2015-12-30 00:31:16 +0100

    missing file added

---
+++ src/main/java/pl/gebickionline/news/NewsManager.java
@@ -0,0 +1,51 @@
+package pl.gebickionline.news;
+
+
+import org.jetbrains.annotations.NotNull;
+import pl.gebickionline.communication.*;
+
+import java.util.List;
+
+public class NewsManager {
+    private static NewsManager instance;
+    private CommunicationManager communicationManager;
+
+    private NewsManager() {
+        communicationManager = CommunicationManagerImpl.getInstance();
+    }
+
+    public long addNews(@NotNull String title, @NotNull String content) {
+        return communicationManager.addNews(title, content);
+    }
+
+    public void modifyNews(long newsId, @NotNull String title, @NotNull String content) {
+        communicationManager.modifyNews(newsId, title, content);
+    }
+
+    public void deleteNews(long newsId) {
+        communicationManager.deleteNews(newsId);
+    }
+
+    public List<News> getNewsList() {
+        return communicationManager.getNewsList();
+    }
+
+    public void turnOnNewsVisibility() {
+        communicationManager.turnOnNewsVisibility();
+    }
+
+    public void turnOffNewsVisibility() {
+        communicationManager.turnOffNewsVisibility();
+    }
+
+    public NewsVisibility getNewsVisibility() {
+        return communicationManager.getNewsVisibility();
+    }
+
+    public static NewsManager getInstance() {
+        if (instance == null)
+            instance = new NewsManager();
+
+        return instance;
+    }
+}
